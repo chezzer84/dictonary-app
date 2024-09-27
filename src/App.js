eadb2c013e0c9f2d6e8df87e9d5f5e15dba7a7b0
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-09-27 14:43:17 +0800

    added logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import logo from "./logo.png";
 import "./App.css";
 
-function App() {
-  return hello;
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
 }
-
-export default App;
