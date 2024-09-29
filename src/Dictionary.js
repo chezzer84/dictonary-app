e0f275346b194fc61b7c2ae6244abe7e072e63e3
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-09-29 11:28:49 +0800

    added description and results from api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
+    console.log(response.data);
+    console.log(response.data.name);
     console.log(response.data.meanings[0]);
+    console.log(response.data.meanings[0].definition);
+
+    setResults(response.data);
   }
 
   function search(event) {
@@ -26,6 +33,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  console.log(props.meaning.partOfSpeech);
+  console.log(props.meaning.definition);
+
+  return (
+    <div className="Meaning">
+      <h4 className="mb-1">{props.meaning.partOfSpeech}</h4>
+      <p className="definition mb-0">{props.meaning.definition}</p>
+    </div>
+  );
+}
