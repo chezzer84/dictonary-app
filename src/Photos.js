93f33d540f42a840900c41f5a5a5d3c3f9b86158
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-10-13 14:55:45 +0800

    added bootstrap and corrected class name

--- src/Photos.js
+++ src/Photos.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Photos.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="photos">
-        <div classname="row">
+        <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4" key={index}>
+              <div className="col-4 mb-3" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
